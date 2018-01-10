e613262a9014272e950398fb9dfd071da466f796
Ilafi Firsta Putri <firstaputri@gmail.com>
2018-01-10 21:04:40 +0700

    create UI

--- UAS-KompasV1/Form1.Designer.cs
+++ UAS-KompasV1/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDeleteStopWords = new System.Windows.Forms.Button();
+            this.linkNews = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(12, 12);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(100, 20);
+            this.txtQuery.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(118, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 230);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(307, 148);
+            this.textBox.TabIndex = 3;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(336, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(298, 368);
+            this.webBrowser.TabIndex = 4;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(657, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(240, 366);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // btnDeleteStopWords
+            // 
+            this.btnDeleteStopWords.Location = new System.Drawing.Point(201, 12);
+            this.btnDeleteStopWords.Name = "btnDeleteStopWords";
+            this.btnDeleteStopWords.Size = new System.Drawing.Size(118, 23);
+            this.btnDeleteStopWords.TabIndex = 6;
+            this.btnDeleteStopWords.Text = "Hapus Stop Words";
+            this.btnDeleteStopWords.UseVisualStyleBackColor = true;
+            this.btnDeleteStopWords.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // linkNews
+            // 
+            this.linkNews.FormattingEnabled = true;
+            this.linkNews.Location = new System.Drawing.Point(12, 48);
+            this.linkNews.Name = "linkNews";
+            this.linkNews.Size = new System.Drawing.Size(307, 173);
+            this.linkNews.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 390);
+            this.Controls.Add(this.linkNews);
+            this.Controls.Add(this.btnDeleteStopWords);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtQuery);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnDeleteStopWords;
+        private System.Windows.Forms.ListBox linkNews;
     }
 }
 
